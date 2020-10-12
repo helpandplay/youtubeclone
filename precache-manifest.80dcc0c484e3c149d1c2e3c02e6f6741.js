52b815857744c106e4159181f2ce0cc91ee07320
helpandplay <helpandplay.kr@gmail.com>
2020-10-12 17:14:50 +0900

    Updates

---
+++ precache-manifest.80dcc0c484e3c149d1c2e3c02e6f6741.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78c85706e54731bd36a9b56defc9ea49",
+    "url": "/youtubeclone/index.html"
+  },
+  {
+    "revision": "f64dd141fbe9d5a15c49",
+    "url": "/youtubeclone/static/css/main.bbcf9212.chunk.css"
+  },
+  {
+    "revision": "468d292b05e2b9fb4910",
+    "url": "/youtubeclone/static/js/2.c3acb3d8.chunk.js"
+  },
+  {
+    "revision": "870267bc23303939d9ca31cdf878e7b3",
+    "url": "/youtubeclone/static/js/2.c3acb3d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f64dd141fbe9d5a15c49",
+    "url": "/youtubeclone/static/js/main.fd43dc00.chunk.js"
+  },
+  {
+    "revision": "97b3198750a554eb89dd",
+    "url": "/youtubeclone/static/js/runtime-main.71fc60f5.js"
+  }
+]);
